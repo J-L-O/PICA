62f623b132c2e583ef974ee877e43aafdee8a180
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2021-02-04 20:09:37 +0100

    Test

--- main.py
+++ main.py
@@ -132,8 +132,8 @@ def main():
     net = net.to(cfg.device)
 
     if int(os.environ["WORLD_SIZE"]) > 1:
-        net = torch.nn.parallel.DistributedDataParallel(net, device_ids=[cfg.local_rank], find_unused_parameters=True,
-                                                        output_device=cfg.local_rank, broadcast_buffers=False).cuda()
+        net = torch.nn.parallel.DistributedDataParallel(net, device_ids=[cfg.local_rank]
+                                                        output_device=cfg.local_rank).cuda()
 
     # tensorboard wrtier
     writer = SummaryWriter(cfg.debug, log_dir=cfg.tfb_dir)
