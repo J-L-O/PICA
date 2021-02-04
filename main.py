6c8905bb56a63735ea1f02aff407ea7b20af03b0
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2021-02-04 20:34:02 +0100

    Fix missing comma

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ def main():
     net = net.to(cfg.device)
 
     if int(os.environ["WORLD_SIZE"]) > 1:
-        net = torch.nn.parallel.DistributedDataParallel(net, device_ids=[cfg.local_rank]
+        net = torch.nn.parallel.DistributedDataParallel(net, device_ids=[cfg.local_rank],
                                                         output_device=cfg.local_rank).cuda()
 
     # tensorboard wrtier
