fae126bb775af4228ab68e5ea1cd918a7a207e1a
Jona Otholt <18723283+J-L-O@users.noreply.github.com>
2021-03-10 13:27:49 +0100

    Test

--- pica/networks.py
+++ pica/networks.py
@@ -27,7 +27,7 @@ class BasicBlock(nn.Module):
 
     self.conv1 = conv3x3(inplanes, planes, stride)
     self.bn1 = nn.BatchNorm2d(planes, track_running_stats=track_running_stats)
-    self.relu = nn.ReLU()
+    self.relu = nn.ReLU(inplace=True)
     self.conv2 = conv3x3(planes, planes)
     self.bn2 = nn.BatchNorm2d(planes, track_running_stats=track_running_stats)
     self.downsample = downsample
@@ -37,16 +37,16 @@ class BasicBlock(nn.Module):
     residual = x
 
     out = self.conv1(x)
-    out = self.bn1(out)
+    # out = self.bn1(out)
     out = self.relu(out)
 
     out = self.conv2(out)
-    out = self.bn2(out)
+    # out = self.bn2(out)
 
     if self.downsample is not None:
       residual = self.downsample(x)
 
-    out = out + residual
+    out += residual
     out = self.relu(out)
 
     return out
